30f17b10ac47a2de558bf3680b4f972577f33dac
falcaomm <manu.m.falcao@gmail.com>
2023-02-10 23:34:04 -0300

    feat - integração com api

--- pokedex/src/context/GlobalState.js
+++ pokedex/src/context/GlobalState.js
@@ -1,6 +1,17 @@
 import { GlobalContext } from "./GlobalContext"
+import { useState, useEffect } from "react";
+import { getPokeList } from "../api/getPokeList";
+import { BASE_URL } from "../constants/url";
 
 export const GlobalState = (props) => {
+
+    const [pokeList, setPokeList] = useState([]);
+
+    useEffect(() => {
+        getPokeList(BASE_URL, setPokeList);
+    }, []);
+
+    console.log(pokeList);
 
     const context = {}
     
