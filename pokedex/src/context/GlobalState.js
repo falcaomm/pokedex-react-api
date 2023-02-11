915633d36523ad000b2533fec71c0a5f1a706e97
falcaomm <manu.m.falcao@gmail.com>
2023-02-10 22:51:07 -0300

    fix - problemas context

--- pokedex/src/context/GlobalState.js
+++ pokedex/src/context/GlobalState.js
@@ -1,1 +1,12 @@
 import { GlobalContext } from "./GlobalContext"
+
+export const GlobalState = (props) => {
+
+    const context = {}
+    
+    return (
+        <GlobalContext.Provider value={context}>
+            {props.children}
+        </GlobalContext.Provider>
+    )
+}
--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import { GlobalStyle } from "./GlobalStyles.styled";
+import { GlobalStyle } from "./GlobalStyles";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
